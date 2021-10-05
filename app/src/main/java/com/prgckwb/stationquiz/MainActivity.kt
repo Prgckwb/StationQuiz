df11a40550d40726de8ebcc47fe74d214e56a6eb
Prgckwb <ckwb.uec@gmail.com>
2021-10-06 05:35:02 +0900

    add: some file

--- app/src/main/java/com/prgckwb/stationquiz/MainActivity.kt
+++ app/src/main/java/com/prgckwb/stationquiz/MainActivity.kt
@@ -3,11 +3,17 @@ package com.prgckwb.stationquiz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.prgckwb.stationquiz.ui.theme.StationQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,11 +23,30 @@ class MainActivity : ComponentActivity() {
             StationQuizTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "welcome"){
+                        composable("welcome"){ WelcomeScreen(navController = navController)}
+                        composable("secondScreen"){ SecondScreen()}
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun WelcomeScreen(navController: NavController){
+    Column {
+        Text(text = "Welcome!")
+        Button(onClick = { navController.navigate("secondScreen") }) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(){
+    Text(text = "Second screen!")
 }
 
 @Composable
