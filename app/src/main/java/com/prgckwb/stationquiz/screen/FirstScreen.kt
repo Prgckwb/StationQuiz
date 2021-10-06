002e4c50221669113ef550e59de3c6dbb54d518c
Prgckwb <ckwb.uec@gmail.com>
2021-10-06 22:35:47 +0900

    add: タイトルに画像を追加

--- app/src/main/java/com/prgckwb/stationquiz/screen/FirstScreen.kt
+++ app/src/main/java/com/prgckwb/stationquiz/screen/FirstScreen.kt
@@ -1,21 +1,34 @@
 package com.prgckwb.stationquiz.screen
 
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.animateSizeAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.prgckwb.stationquiz.R
 
 // タイトル画面の組み立てコンポーザブル
 @Composable
 fun DisplayTitleScreen(navController: NavController) {
-    Column {
+    Column(
+    ) {
         TitleText()
+        Image(painter = painterResource(id = R.drawable.wabu_star),
+            contentDescription = "わぶすたー",
+            alignment = Alignment.Center
+        )
         StartButton(navController = navController)
     }
 }
@@ -27,7 +40,8 @@ fun TitleText(){
         style = MaterialTheme.typography.h1,
         modifier = Modifier
             .padding(32.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .fillMaxHeight(0.3f)
     )
 }
 
@@ -41,10 +55,12 @@ fun StartButton(navController: NavController){
     ) {
         Text(
             text = "GAME START",
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.fillMaxHeight(0.3f),
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
