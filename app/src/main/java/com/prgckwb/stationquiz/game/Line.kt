c44fe47a16ba60c6fa6be4d7bf20b4db5c2ce924
Prgckwb <ckwb.uec@gmail.com>
2021-10-06 21:53:00 +0900

    add: 方面要素を取り入れるための関数を追加

--- app/src/main/java/com/prgckwb/stationquiz/game/Line.kt
+++ app/src/main/java/com/prgckwb/stationquiz/game/Line.kt
@@ -1,7 +1,15 @@
 package com.prgckwb.stationquiz.game
 
 data class Line(val stations :List<Station>, val lineName: String) {
-        fun random(): Station{
-            return stations.random()
-        }
+    val firstStation = stations[0]
+    val lastStation = stations[stations.size-1]
+
+    fun random(): Station{
+        return stations.random()
+    }
+
+    fun getLineDirectText(direction: Int): String{
+        val arrow = if(direction > 0) "->" else "<-"
+        return "${firstStation.name} $arrow ${lastStation.name}"
+    }
 }
